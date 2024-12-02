740ca016fddead55db16108952f6ad658090d8ff
mustafa276 <mmustafamajeed1@gmail.com>
2024-12-02 14:56:09 +0500

    login

---
+++ activity2.py
@@ -0,0 +1,8 @@
+from tkinter import *
+root = Tk()
+root.title('login app')
+root.geometry('400x400')
+frame = Frame(master=root, height = 200, width = 360, bg = '#d0efff')
+lbl1 = Label(frame, text = 'name', bg = '#3895D3', fg = 'white', width = 12)
+lbl2 = Label(frame, text = 'email address', bg = '#3895D3', fg = 'white', width = 12)
+lbl3 = Label(frame, text = 'password', bg = '#3895D3', fg = 'white', width = 12)
